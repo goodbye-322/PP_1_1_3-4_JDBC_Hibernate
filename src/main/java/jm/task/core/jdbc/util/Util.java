1809983187404dc0bea1997f6f07c0810789ebc7
Vlad <124003398+goodbye-322@users.noreply.github.com>
2023-07-11 19:59:11 +0300

    Add files via upload

---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,33 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    // реализуйте настройку соеденения с БД
+    private static Connection connection = null;
+    private static Util instance = null;
+    private final static String URL = "jdbc:mysql://localhost:3306/user";
+    private final static String USERNAME = "root";
+    private final static String PASSWORD = "root";
+
+    public static Connection getConnection() {
+        try {
+            if (null == connection || connection.isClosed()) {
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+    public static void closeConnection() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
